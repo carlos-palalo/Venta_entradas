13c8ddd5e196b1569920abfd9082f07d5b5bec43
carlos-palalo <carlos-palalo@gmail.com>
2020-12-03 20:40:21 +0100

    actu axarnet

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,5 +1,5 @@
 <?php
-define('DNS','mysql:dbname=conciertos;host=localhost');  
+define('DNS','mysql:dbname=conciertos;host=node29133-env-1800697.es-1.axarnet.cloud');  
 define('USERNAME','root');  
 define('PASSWORD','toor');
 ?>
